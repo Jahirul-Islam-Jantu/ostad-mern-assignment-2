0ac9d611916069f596c066d694d2b414b72ab390
Jahirul Islam <jahirulislamjantu002@gmail.com>
2024-02-02 22:56:26 +0600

    fs system added

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const http = require("http");
 const server = http.createServer((req, res) => {
   if (req.url == "/") {
@@ -12,6 +13,18 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/html" });
     res.write("<h1>This is Contact Page</h1>");
     res.end();
+  } else if (req.url == "/file-write") {
+    fs.writeFile("demo.txt", "Hello World", () => {
+      if (error) {
+        res.writeHead(400, { "Content-Type": "text/html" });
+        res.write("<h1>File write not successfully</h1>");
+        res.end();
+      } else {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write("<h1>File write successfully</h1>");
+        res.end();
+      }
+    });
   }
 });
 server.listen(5500, () => {
