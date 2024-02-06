1d1a392a5b2e27025277f63e0432e5f41a0ce26e
Jahirul Islam <jahirulislamjantu002@gmail.com>
2024-02-06 22:38:08 +0600

    multer

--- index.js
+++ index.js
@@ -37,28 +37,7 @@ const server = http.createServer((req, res) => {
         res.end();
       }
     });
-  } else if (req.url == "/upload") {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write(
-      <form action="/stats" enctype="multipart/form-data" method="post">
-        <div class="form-group">
-          <input type="file" class="form-control-file" name="uploaded_file" />
-          <input
-            type="text"
-            class="form-control"
-            placeholder="Number of speakers"
-            name="nspeakers"
-          />
-          <input
-            type="submit"
-            value="Get me the stats!"
-            class="btn btn-default"
-          />
-        </div>
-      </form>
-    );
   }
-  res.end();
 });
 
 server.listen(5500, () => {
